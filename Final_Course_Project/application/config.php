a03ec7488fe29348721e9c1010b6a26e312602a8
AlexWeber <aw118@iu.edu>
2022-03-29 13:59:52 -0400

    we are screwed v2

---
+++ Final_Course_Project/application/config.php
@@ -0,0 +1,13 @@
+<?php
+/*** Author: your name*
+ * Date: 3/29/2022*
+ * File: config.php*
+ * Description: */
+//error reporting level: 0 to turn off all error reporting; E_ALL to report all
+error_reporting(E_ALL);
+
+//local time zone
+date_default_timezone_set('America/New_York');
+
+//base url of the application
+define("BASE_URL", "http://localhost/ComposserProjects/Final_Course_Project");
