4704702e6cbc40cd2bb766b3008534c1b050423e
AlexWeber <aw118@iu.edu>
2022-03-29 14:05:19 -0400

    fixing this github stuff

---
+++ Final_Course_Project/controllers/controllys.php
@@ -0,0 +1,5 @@
+<?php
+/*** Author: your name*
+ * Date: 3/29/2022*
+ * File: controllys.php*
+ * Description: */
---
+++ Final_Course_Project/views/view.example.php
@@ -0,0 +1,5 @@
+<?php
+/*** Author: your name*
+ * Date: 3/29/2022*
+ * File: view.example.php*
+ * Description: */
